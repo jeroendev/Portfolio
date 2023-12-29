603713b3a2fb5da577b2d6fc738cb83c80d4a1d3
jeroendev <45533167+jeroendev@users.noreply.github.com>
2023-12-29 15:25:39 +0100

    v1

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,26 +7,30 @@ const Skills = () => {
     <section className="skills__section bg-body-secondary" id='skills'>
         <Container>
     <div className='skills__container'>
-        <h1 className="skills__title">Tech Stack 
-        <span className='skill1'> | HTML5 & CSS3</span>
-        <span className='skill2'> | JavaScript & TypeScript</span>
-        <span className='skill3'> | Bootstrap & Tailwind CSS</span>
-        <span className='skill4'> | React & Vite</span>
-        </h1>
+        <h1 className="skills__title">Tech Stack</h1>
+        <span className='skill1'> HTML5 & CSS3</span>
+        <span className='skill2'> JavaScript & TypeScript</span>
+        <span className='skill3'> Bootstrap & Tailwind CSS</span>
+        <span className='skill4'> React & Vite</span>
+        
 
         <div className='skills__logos'>
             <div className='parent__skills'>
+              <div className="skillset1">
             <div className='skills__card card1'>
                 <img className='skills__img' src="https://skillicons.dev/icons?i=html,css" alt="HTML and CSS" />
               </div>
               <div className='skills__card card2'>
                 <img className='skills__img' src="https://skillicons.dev/icons?i=js,ts" alt="JavaScript and TypeScript" />
               </div>
+              </div>
+              <div className="skillset2">
               <div className='skills__card card3'>
                 <img className='skills__img' src="https://skillicons.dev/icons?i=bootstrap,tailwind" alt="Bootstrap and Tailwind CSS" />
               </div>
               <div className='skills__card card4'>
                 <img className='skills__img' src="https://skillicons.dev/icons?i=react,vite" alt="React and Vite" />
+              </div>
               </div>
             </div>
         </div>
