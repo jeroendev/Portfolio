d1d8b0557965196f638bd9ec3dab109499cbb34a
jeroendev <45533167+jeroendev@users.noreply.github.com>
2024-01-20 06:34:55 +0100

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import './App.css';
-import BasicNavbar from './components/header/Navbar';
-import Home from './components/home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import About from './components/about/About'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+import "./App.css";
+import BasicNavbar from "./components/header/Navbar";
+import Home from "./components/home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
-
   return (
     <>
-    <BasicNavbar />
-    <main className='main'>
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </main>
+      <BasicNavbar />
+      <main className="main">
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+        <SpeedInsights />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
