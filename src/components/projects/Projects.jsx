572c22093c917fb1ca972b0261bafc21f4e7e38a
jeroendev <45533167+jeroendev@users.noreply.github.com>
2024-01-31 19:22:09 +0100

    v2

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
         <ProjectCard
           imageUrl={"../screencapture-limoncello.png"}
           description1={
-            "My recent project is an engaging platform dedicated to the sales and production of delightful limoncello. Developed using React and Bootstrap, this dynamic website provides an intuitive and visually appealing user experience. The site features comprehensive information about the range of limoncello and the story behind Zesty Spirits. One unique aspect of this project is the integration of AI in the creation of compelling visuals. The images on the website, showcasing the enticing limoncello products, were generated with the assistance of artificial intelligence."
+            "My recent project is an engaging platform dedicated to the sales of delightful limoncello. This website provides an intuitive and visually appealing user experience. One unique aspect of this project is the integration of AI in the creation of compelling visuals. The images on the website, showcasing the enticing limoncello products, were generated with the assistance of artificial intelligence."
           }
           description2={
             "The website is designed with a focus on responsiveness and modern design, ensuring seamless navigation across various devices. It has provided me with an opportunity to enhance both my design and programming skills, and I look forward to the continued refinement and expansion of the project."
